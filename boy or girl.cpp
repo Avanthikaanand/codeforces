9009b842b3fd52a1dfedf57e37d30ec71320262c
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2021-03-10 17:50:19 +0530

    Create boy or girl.cpp

---
+++ boy or girl.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+    #include <cstring>
+    #include <algorithm>
+    using namespace std;
+    int main () 
+    {
+    string s;
+    int c=0;
+    cin>>s;
+    sort(s.begin(),s.end());
+    for (int i=0;i<s.size();i++)
+    {
+       if (s[i]!=s[i+1])c++;
+    }
+    if (c%2==0)cout<<"CHAT WITH HER!";
+    else cout<<"IGNORE HIM!";
+    return 0;
+    }
