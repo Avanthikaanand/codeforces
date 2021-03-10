a7c085b5c1f14406c00e208444db11a22922ea21
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2021-03-10 16:23:15 +0530

    Create domino piling.cpp

---
+++ domino piling.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+        using namespace std;
+        int main()
+        {
+                 int x,y;
+                 double c;
+                 cin>>x>>y;
+                 c=(x*y)/2;
+                 cout<<int(c);
+                 return 0;
+			 }
