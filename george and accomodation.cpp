d6e7a49d2c78eb6ad3dfd8e8f8fcf73f9470fc51
Avanthika Anand <80388135+Avanthikaanand@users.noreply.github.com>
2021-03-12 17:38:53 +0530

    Create george and accomodation.cpp

---
+++ george and accomodation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+         int n,a,b,c=0,i;
+         cin>>n;
+         if(i=0;i<n;i++)
+         {
+                  cin>>a>>b;
+                  if(b-a)>=2)
+                  {
+                           c++;
+                  }
+         }
+         cout<<c;
+         return 0;
+}
